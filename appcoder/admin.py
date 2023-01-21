f1fb6d604e87d40ab18e8d3b7dddc462e4f701dd
Lucjana <84865236+Lucjana@users.noreply.github.com>
2023-01-21 09:52:42 -0300

    Add files via upload

--- appcoder/admin.py
+++ appcoder/admin.py
@@ -5,5 +5,5 @@ admin.site.register(Curso)
 admin.site.register(Estudiante)
 admin.site.register(Profesor)
 admin.site.register(Entregable)
-
+admin.site.register(Avatar)
 # Register your models here.
--- appcoder/urls.py
+++ appcoder/urls.py
@@ -15,4 +15,22 @@ urlpatterns = [
     path("busquedaComision/", busquedaComision, name="busquedaComision"),
     path("buscar/", buscar, name="buscar"),
 
+    path("profesores", profesores, name="profesores"),
+    path("eliminarProfesor/<id>", eliminarProfesor, name="eliminarProfesor"),
+    path("editarProfesor/<id>", editarProfesor, name="editarProfesor"),
+
+    path("estudiante/list/", EstudianteList.as_view(), name="estudiante_list"),
+    path('estudiante/nuevo/', EstudianteCreacion.as_view(), name='estudiante_crear'),
+    path('estudiante/<pk>', EstudianteDetalle.as_view(), name='estudiante_detalle'),
+    path('estudiante/borrar/<pk>', EstudianteDelete.as_view(),
+         name='estudiante_borrar'),
+    path('estudiante/editar/<pk>', EstudianteUpdate.as_view(),
+         name='estudiante_editar'),
+
+    path("register/", register, name="register"),
+    path("login/", login_request, name="login"),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('editarPerfil/', editarPerfil, name='editarPerfil'),
+    path('agregarAvatar/', agregarAvatar, name='agregarAvatar'),
+
 ]
